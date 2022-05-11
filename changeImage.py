2b121c2e2e785199403772933d563982cc408a33
Andres Murillo <andres.murillo@outlook.com>
2022-05-11 00:07:33 -0600

    Modified file changeImage, using f-string and creating functions: main and convert_images.

--- changeImage.py
+++ changeImage.py
@@ -5,8 +5,18 @@ from PIL import Image
 
 images_path = '/home/amurillo/workspace/finalproject_Coursera/supplier-data/images/'
 
-for f in os.listdir(images_path):
-    if f.endswith('.tiff'):
-        i = Image.open(images_path + f)
-        fname, fext = os.path.splitext(f)
-        i.convert('RGB').resize((600, 400)).save(f'/home/amurillo/workspace/finalproject_Coursera/supplier-data/images/{fname}.jpeg')
+def convert_images(path):
+    ''' Converts .tiff images into .jpeg files,
+        also resizes files to 600 x 400 pixels.
+    '''
+    for f in os.listdir(path):
+        if f.endswith('.tiff'):
+            i = Image.open(path + f)
+            fname, fext = os.path.splitext(f)
+            i.convert('RGB').resize((600, 400)).save(path + f'{fname}.jpeg')
+
+def main():
+    convert_images(images_path)
+
+if __name__ == '__main__':
+    main()
