60b2344ab46f6fa86d412ccc8e57bceead2b4af6
andres-murillo <andres.murillo@outlook.com>
2022-05-30 00:26:34 -0600

    Updates to run.py and supplier_image_upload.
    Updated run.py file, added the jpeg image name to dictionary, still
    pending to format the weight.
    Completed the supplier_image_upload file, pointed tto test url.

--- run.py
+++ run.py
@@ -3,28 +3,34 @@
 import os
 import requests
 
-filepath = '/home/amurillo/pruebas/files/'
+img_path = '/home/amurillo/workspace/finalproject_Coursera/supplier-data/images/'
+desc_path = '/home/amurillo/workspace/finalproject_Coursera/supplier-data/descriptions/'
 
-filelist = os.listdir(filepath)
+desc_filelist = os.listdir(desc_path)
 
-payload = {'title': '', 'name': '', 'date': '', 'feedback': ''}
+# print(desc_filelist)
 
-keys = ['title', 'name', 'date', 'feedback']
+payload = {'name': '', 'weight': '', 'description': '', 'image_name':''}
 
-def post_dict():
+keys = ['name', 'weight', 'description', 'image_name']
+
+def post_dict(filelist, filepath):
     for textfile in filelist:
+        img_name = textfile.replace('.txt', '.jpeg')
         file = filepath + textfile
         with open(file, 'r') as f:
             keynumber = 0
             for line in f.readlines():
                 payload[keys[keynumber]] = line.strip()
                 keynumber += 1
-        resp = requests.post('https://example.com/path/to/api', data=payload)
-        print(f'Status code: {resp.status_code}')
-        print(resp.text)
+        # resp = requests.post('https://example.com/path/to/api', data=payload)
+        # print(f'Status code: {resp.status_code}')
+        # print(resp.text)
+        payload['image_name'] = img_name
+        print(payload)
 
 def main():
-    post_dict()
+    post_dict(desc_filelist, desc_path)
 
 if __name__ == '__main__':
     main()
--- supplier_image_upload.py
+++ supplier_image_upload.py
@@ -9,6 +9,7 @@ url = 'http://demozilla.examplehostingprovider.net/'
 images_path = '/home/amurillo/workspace/finalproject_Coursera/supplier-data/images/'
 
 def upload_images(path):
+    '''Uploads jpg images to url.'''
     for f in os.listdir(path):
         if f.endswith('.jpeg'):    
             file_complete = path + f
@@ -16,6 +17,7 @@ def upload_images(path):
                 r = requests.post(url, files={'file': opened})
 
 def main():
+    ''' Main '''
     upload_images(images_path)
 
 if __name__ == '__main__':
