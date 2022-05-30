358919176a87e2fb094a27836f46e2ee84889188
andres-murillo <andres.murillo@outlook.com>
2022-05-29 20:41:18 -0400

    Deleted test file manipulate_images.py, and modified supplier_image_upload.py to upload files to test url.

--- supplier_image_upload.py
+++ supplier_image_upload.py
@@ -1,9 +1,22 @@
 #!/usr/bin/env python3
+
+import os
 import requests
 
-# This example shows how a file can be uploaded using
-# The Python Requests module
 
-url = "http://localhost/upload/"
-with open('/usr/share/apache2/icons/icon.sheet.png', 'rb') as opened:
-    r = requests.post(url, files={'file': opened})
+# url = "http://localhost/upload/"
+url = 'http://demozilla.examplehostingprovider.net/'
+images_path = '/home/amurillo/workspace/finalproject_Coursera/supplier-data/images/'
+
+def upload_images(path):
+    for f in os.listdir(path):
+        if f.endswith('.jpeg'):    
+            file_complete = path + f
+            with open(file_complete, 'rb') as opened:
+                r = requests.post(url, files={'file': opened})
+
+def main():
+    upload_images(images_path)
+
+if __name__ == '__main__':
+    main()
