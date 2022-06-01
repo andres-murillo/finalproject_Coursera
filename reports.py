3a3d601187d83a840c489ea305d218b3472e996b
andres-murillo <andres.murillo@outlook.com>
2022-06-01 00:56:46 -0600

    Continued configuring reports.py

--- reports.py
+++ reports.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 
+import os
 from reportlab.platypus import SimpleDocTemplate, Paragraph, Spacer, Table, Image
 from reportlab.lib.styles import getSampleStyleSheet
 from datetime import date
@@ -16,8 +17,18 @@ items = {'name': '', 'weight': ''}
 
 def items_weight(filelist, filepath):
     for textfile in filelist:
-        
+        file = filepath + textfile
+        with open(file, 'r')  as f:
+            keynumber = 0
+            for line in f.readlines():
+                print(line)
 
 
 def generate_report(attachment, title, paragraph):
     pass
+
+def main():
+    items_weight(desc_filelist, desc_path)
+
+if __name__ == '__main__':
+    main()
