515611016d0f0591d6ec4fff342b4728f27a197d
andres-murillo <andres.murillo@outlook.com>
2022-05-31 21:00:21 -0400

    Added date for rpt title, still building data for report.

--- reports.py
+++ reports.py
@@ -2,11 +2,22 @@
 
 from reportlab.platypus import SimpleDocTemplate, Paragraph, Spacer, Table, Image
 from reportlab.lib.styles import getSampleStyleSheet
+from datetime import date
 
+desc_path = '/home/amurillo/workspace/finalproject_Coursera/supplier-data/descriptions/'
+desc_filelist = os.listdir(desc_path)
 
+today = date.today().strftime('%B %d, %Y')
 styles = getSampleStyleSheet()
 report = SimpleDocTemplate('processed.pdf')
-report_title = Paragraph('Processed Update on ', styles['h1'])
+report_title = Paragraph(f'Processed Update on {today}', styles['h1'])
+
+items = {'name': '', 'weight': ''}
+
+def items_weight(filelist, filepath):
+    for textfile in filelist:
+        
+
 
 def generate_report(attachment, title, paragraph):
     pass
