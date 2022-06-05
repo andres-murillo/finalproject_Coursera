916c0c64e0e803e7285a23e685de90252fe223be
andres-murillo <andres.murillo@outlook.com>
2022-06-04 23:30:40 -0400

    Advanced with reports.py configuration, added processed.pdf file.~

--- reports.py
+++ reports.py
@@ -13,22 +13,37 @@ styles = getSampleStyleSheet()
 report = SimpleDocTemplate('processed.pdf')
 report_title = Paragraph(f'Processed Update on {today}', styles['h1'])
 
-items = {'name': '', 'weight': ''}
-
 def items_weight(filelist, filepath):
+    fruits = {}
+    fruta = ''
+    table_data = []
     for textfile in filelist:
         file = filepath + textfile
+        keynumber = 0
         with open(file, 'r')  as f:
-            keynumber = 0
             for line in f.readlines():
-                print(line)
+                if keynumber <= 1:
+                    if keynumber == 0:
+                        fruta = line.strip()
+                    else:
+                        fruits[fruta] = line.strip()
+                    keynumber += 1
+    print(fruits)
+    for k, v in fruits.items():
+        name_fruit = 'name: ' + k
+        weight_fruit = 'weight: ' + v
+        table_data.append([name_fruit, weight_fruit])
+    report_table = Table(data=table_data)
+    return report_table
 
-
-def generate_report(attachment, title, paragraph):
-    pass
+# def generate_report(attachment, title, paragraph):
+#     pass
 
 def main():
-    items_weight(desc_filelist, desc_path)
+    # items_weight(desc_filelist, desc_path)
+    reporte_tabla = items_weight(desc_filelist, desc_path)
+    # print(reporte_tabla)
+    report.build([report_title, reporte_tabla])
 
 if __name__ == '__main__':
     main()
